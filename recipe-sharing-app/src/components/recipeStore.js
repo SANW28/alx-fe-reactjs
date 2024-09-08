76223d37915654ab5bdb585e56c148f9ca1b6cfe
SANW28 <abenaattakruh@gmail.com>
2024-09-08 22:43:11 +0000

    issues resloved

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,6 +1,26 @@
 
 import { create } from 'zustand';
 
+/*
+import { create } from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],  
+
+  
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],  // Add new recipe to existing list
+    })),
+
+  / 
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export default useRecipeStore;*/
+
+
+
 const useRecipeStore = create((set) => ({
   recipes: [],
   
