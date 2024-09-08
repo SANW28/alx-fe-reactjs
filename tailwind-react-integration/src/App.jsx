4d18d353753d31d52443579c2cac2015ad055ca7
SANW28 <abenaattakruh@gmail.com>
2024-09-08 22:59:21 +0000

    first commit

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import tailwindConfig from '../tailwind.config'
+import UserProfile from './components/UserProfile'
+
+function App() {
+
+  return 
+}
+
+export default App;
