4ac7fce0004c26e8e241129a80aab68e26be2cf1
SANW28 <abenaattakruh@gmail.com>
2024-10-13 18:57:37 +0000

    hover effect add

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const UserProfile = ({ user }) => {
   return (
-    <div className=" md:p-8 sm:p-4 sm:max-w-xs md:max-w-sm mx-auto">
+    <div className=" md:p-8 sm:p-4 sm:max-w-xs md:max-w-sm mx-auto hover:shadow-xl">
       <div className="flex flex-col items-center">
         {/* Responsive Image */}
         <img
           src={user.profileImage}
           alt={user.name}
-          className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full object-cover mb-4"
+          className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full object-cover mb-4 hover:scale-110 transition-transform duration-300 ease-in-out"
         />
         {/* Responsive Typography */}
-        <h1 className="sm:text-lg md:text-xl font-bold text-gray-800">
+        <h1 className="sm:text-lg md:text-xl font-bold text-gray-800 hover:text-blue-500">
           {user.name}
         </h1>
         <p className="text-sm sm:text-base text-gray-600 text-center">
